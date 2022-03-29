b658d915203d768e414dc6ad76d10578634118a5
Pascal <85689562+pascal13ch@users.noreply.github.com>
2022-03-29 13:47:36 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,33 +12,3 @@ variable "instance_hostname_prefix" {
     description = "Hostname Prefix"
     default     = "ubuntu"
 }
-
-variable "instance_count" {
-  description   = "number of VMs to create"
-  type          = number
-  default       = 1
-  validation {
-    condition       = var.vm_count >= 1
-    error_message   = "Der Wert muss mindestens 1 sein."
-  }
-}
-
-variable "instance_cpu" {
-  description   = "number of CPUs per VM"
-  type          = number
-  default       = 1
-  validation {
-    condition       = var.vm_cpu >= 1
-    error_message   = "Der Wert muss mindestens 1 sein."
-  }
-}
-
-variable "instance_mem" {
-  description   = "number of GBit Memory per VM"
-  type          = number
-  default       = 1
-  validation {
-    condition       = var.vm_cpu >= 1
-    error_message   = "Der Wert muss mindestens 1 sein."
-  }
-}
