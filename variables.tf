50a64cee199820b3700a97ee3c2e723bb5265a72
Pascal <85689562+pascal13ch@users.noreply.github.com>
2022-03-30 17:09:23 +0200

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+    default = "eu-central-1" # Frankfurt
+}
+
+variable "instance_type" {
+    default = "t2.micro"
+}
+
+variable "instance_name" {
+    default = "Webserver"
+}
+variable "root_password" {
+    default = "Welcome$22"
+}
