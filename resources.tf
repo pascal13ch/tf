4426ccb40c63badc6a581736a5639fe4067fa3ae
Pascal Werren <pascal.werren@ict-campus.net>
2022-03-30 17:35:25 +0200

    added ssh connection with ssh_key

--- resources.tf
+++ resources.tf
@@ -22,10 +22,11 @@ resource "aws_instance" "web" {
   }
 
   connection {
-    type     = "ssh"
-    user     = "root"
-    password = var.root_password
-    host     = self.public_ip
+    type        = "ssh"
+    host        = self.public_ip
+    user        = "ubuntu"
+    private_key = file("${path.module}/aws_key")
+    timeout     = "4m"
   }
 
   provisioner "remote-exec" {
@@ -35,4 +36,39 @@ resource "aws_instance" "web" {
       "apt -y install apache2"
     ]
   }
+}
+
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "aws_key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDak9DLleCQOyn1RtehV5M2B2SrTi1Yfus+MJ1mmuy8rDHWjavfpgZYpwP58iELPqIinhK7HGsgcWhyTJ38wJzZlyMT6CkFL3zo1HSyRGJp8VrE63IAK/lU+JSFHASxEI1WtBAXEKo58xUoO5IvyD44kBGucHVZueKU7e3HzdYHCqf8Jk9eWkkaRoqD3mMZj4j3seyIrO7foEYjqUp1L4YIBUVWWGXX/j1e9fhVgNsfchIDWLsiGHDzuIvQtaz6EdVVqpe6aijOG12hWU6j3WzeFrHdakjVa/Pg2AZG337bFV8GgxVGCbsTPhmK1G6QTO7IFxqO7V4T1hG/p3Owd0k5 post\werrenp@w00vlq"
+}
+
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+  ingress                = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 22
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "tcp"
+      security_groups  = []
+      self             = false
+      to_port          = 22
+    }
+  ]
 }
